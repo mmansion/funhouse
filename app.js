d4263596a7174c353de2232292c6a5bd2d909f8e
Mikhail Mansion <mikhail@transmission.studio>
2023-05-08 02:51:41 -0400

    change port

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 app.use('/', express.static('public'))
 app.get('/favico.ico', (req, res) => {res.sendStatus(404);});
-app.listen(80, () => { console.log(`app started`)})
+app.listen(3000, () => { console.log(`app started`)})
